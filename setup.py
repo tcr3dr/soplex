aed5e28969cb2ab6ac4fe9529ffc5884614014b1
Tim Ryan <tim@3drobotics.com>
2015-06-17 13:43:53 -0700

    Adds setup.py. Rename to soplex

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='soplex',
+      version='1.0',
+      description='',
+      author='Multiplex sockets',
+      author_email='tim@3drobotics.com',
+      url='https://github.com/tcr3dr/soplex/',
+      scripts=['soplex'],
+      )

0daaee5478435a5bdeeda2200918b30dc6f304a0
Tim Ryan <tim@3drobotics.com>
2015-06-17 12:29:14 -0700

    Updates with TCP.

--- piper.py
+++ piper.py
@@ -3,48 +3,40 @@ import sys
 import six
 from gevent import socket
 from gevent.queue import Queue
-from gevent.socket import wait_read
+from gevent.socket import wait_read, wait_write
 import gevent
+import base64
+import json
 
 def parse_arg(arg):
     (proto, role, port) = arg.split(':')
     if proto != 'tcp' and proto != 'udp':
         print('Expected one of tcp or udp', file=sys.stderr)
         sys.exit(1)
-    if role != 'client' and role != 'server':
-        print('Expected one of client or server', file=sys.stderr)
+    if role != 'in' and role != 'out' and role != 'server' and role != 'client':
+        print('Expected one of in or out', file=sys.stderr)
         sys.exit(1)
     return (proto, role, int(port))
 
-args = sys.argv[1:]
-print('piping over', parse_arg(args[0]))
+def udp(idx, role, port):
+    global ports
 
-def udp_client(idx, port, queue):
     address = ('127.0.0.1', port)
     sock = socket.socket(type=socket.SOCK_DGRAM)
-    sock.connect(address)
-    while True:
-        try:
-            wait_read(sock.fileno())
-            data, address = sock.recvfrom(8192)
-            print('%s:%s: got %r' % (address + (data, )))
-            queue.put((idx, data, address))
-        except socket.error as e:
-            print('Socket error:', e)
-            break
 
-def udp_server(idx, port):
-    address = ('127.0.0.1', port)
-    sock = socket.socket(type=socket.SOCK_DGRAM)
-    sock.bind(address)
+    if role == 'in':
+        sock.bind(address)
+
+    in_queue = Queue()
+    out_queue = Queue() 
 
     def read():
         while True:
             try:
                 wait_read(sock.fileno())
                 data, address = sock.recvfrom(port)
-                print('%s:%s: got %r' % (address + (data, )))
-                out_queue.put((data, address))
+                print(role, 'received %s:%s: got %r' % (address + (data, )))
+                in_queue.put((data, address))
             except socket.error as e:
                 print('Socket error:', e)
                 break
@@ -53,23 +45,143 @@ def udp_server(idx, port):
         while True:
             try:
                 wait_write(sock.fileno())
-                msg = in_queue.get()
-                data, address = sock.send(msg, ('127.0.0.1', port))
-                print('%s:%s: got %r' % (address + (data, )))
-                queue.put((data, address))
+                msg = out_queue.get()
+                print(role, 'sending %s' % msg)
+                out_bytes = sock.sendto(msg, address)
             except socket.error as e:
                 print('Socket error:', e)
                 break
 
-    gevent.spawn(read)
-    gevent.spawn(write)
+    def resident():
+        buf = ''
+        while True:
+            pair = in_queue.get()
+            if idx == 0:
+                buf += pair[0]
+                res = buf.split('\n')
+                buf = res.pop()
+                for item in res:
+                    (tidx, b) = json.loads(item)
+                    msg = base64.b64decode(b)
+                    ports[tidx][1].put(msg)
+            else:
+                ports[0][1].put(json.dumps([idx, base64.b64encode(pair[0])]) + '\n')
 
-def udp(idx, type, port, queue):
-    if type == 'client':
-        return udp_client(idx, port, queue)
+
+    if role == 'in':
+        gevent.spawn(read)
+    if role == 'out':
+        gevent.spawn(write)
+    gevent.spawn(resident)
+
+    return (in_queue, out_queue)
+
+
+def tcp(idx, role, port):
+    global ports
+
+    address = ('127.0.0.1', port)
+    sock = socket.socket(type=socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    if role == 'server':
+        sock.bind(address)
+        sock.listen(1)
     else:
-        return udp_server(idx, port, queue)
+        print(address)
+        sock.connect(address)
 
-queue_in = Queue()
-udp(0, parse_arg(args[0])[1], parse_arg(args[0])[2], queue)
+    in_queue = Queue()
+    out_queue = Queue()
+
+    conns = []
+
+    def read_clients():
+        while True:
+            try:
+                wait_read(sock.fileno())
+                conn, addr = sock.accept()
+                conns.append(conn)
+                gevent.spawn(read, conn)
+            except socket.error as e:
+                print('Socket error:', e)
+                break
+
+    def read(conn):
+        while True:
+            try:
+                wait_read(conn.fileno())
+                data = conn.recv(port)
+                print(role, 'received %r' % (data, ))
+                if not data:
+                    break
+                in_queue.put(data)
+            except socket.error as e:
+                print('Socket error:', e)
+                break
+        conn.close()
+        conns.remove(conn)
+
+    def write():
+        while True:
+            try:
+                wait_write(sock.fileno())
+                msg = out_queue.get()
+                print(role, 'sending %s' % msg)
+                out_bytes = sock.send(msg)
+            except socket.error as e:
+                print('Socket error:', e)
+                break
+
+    def write_clients():
+        while True:
+            try:
+                # wait_write(sock.fileno())
+                msg = out_queue.get()
+                print(role, 'sending %s' % msg)
+                for conn in conns:
+                    out_bytes = conn.send(msg)
+            except socket.error as e:
+                print('Socket error:', e)
+                break
+
+    def resident():
+        buf = ''
+        while True:
+            pair = in_queue.get()
+            if idx == 0:
+                buf += pair
+                res = buf.split('\n')
+                buf = res.pop()
+                for item in res:
+                    (tidx, b) = json.loads(item)
+                    msg = base64.b64decode(b)
+                    ports[tidx][1].put(msg)
+            else:
+                ports[0][1].put(json.dumps([idx, base64.b64encode(pair[0])]) + '\n')
+
+
+    if role == 'client':
+        gevent.spawn(read, sock)
+        gevent.spawn(write)
+    if role == 'server':
+        gevent.spawn(read_clients)
+        gevent.spawn(write_clients)
+    gevent.spawn(resident)
+
+    return (in_queue, out_queue)
+
+
+args = sys.argv[1:]
+
+ports = []
+
+for arg in args:
+    (proto, role, port) = parse_arg(arg)
+    if len(ports) == 0:
+        print('piping over', parse_arg(arg))
+    else:
+        print('attached to', parse_arg(arg))
+    ports.append((tcp if proto == 'tcp' else udp)(len(ports), role, port))
+
 gevent.wait()
